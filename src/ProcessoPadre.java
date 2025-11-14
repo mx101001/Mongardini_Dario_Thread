216552d189c6536ef44d26302bc5ee74e35fdb23
MX <dario.mongardini@gmail.com>
2025-11-14 09:16:55 +0100

    Sviluppo funzioni del ProcessoPadre e del ThreadImbottitura

--- src/ProcessoPadre.java
+++ src/ProcessoPadre.java
@@ -1,5 +1,23 @@
 package src;
 
 public class ProcessoPadre {
-    
+    public static void main(String[] args){
+
+         // Creazione dei thread 
+        ThreadImbottitura threadImbottitura = new ThreadImbottitura();
+        ThreadAssemblaggio threadAssemblaggio = new ThreadAssemblaggio();
+
+        // Avvio dei thread in parallelo
+        threadAssemblaggio.start();
+        threadImbottitura.start();
+
+        try {
+             // Attesa del completamento dei thread figli (join)
+            threadAssemblaggio.join();
+            threadImbottitura.join();
+        } catch (InterruptedException e) {
+            System.err.println("Errore durante l'attesa dei thread: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
--- src/ThreadImbottitura.java
+++ src/ThreadImbottitura.java
@@ -1,5 +1,29 @@
 package src;
 
-public class ThreadImbottitura {
-    
+public class ThreadImbottitura extends Thread {
+     public ThreadImbottitura() {
+        super("Imbottitura");
+    }
+
+@Override
+public void run(){
+    try {
+            System.out.println("prima fase");
+            Thread.sleep(800); // Simula il tempo di preparazione materiali
+            
+            System.out.println("seconda fase");
+            Thread.sleep(1200); // Simula il tempo di applicazione primo strato
+            
+            System.out.println("terza fase");
+            Thread.sleep(1000); // Simula il tempo di applicazione secondo strato
+            
+            System.out.println("quarta fase");
+            Thread.sleep(1300); // Simula il tempo di rivestimento
+            
+            System.out.println("quinta fase");
+    } catch (InterruptedException e) {
+        System.err.println("Thread interrotto: " + e.getMessage());
+        Thread.currentThread().interrupt();
+    }
+}
 }
